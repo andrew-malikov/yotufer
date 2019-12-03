3810aa1e35b50212c7e038421ed45ed40effc6e9
markupcode <andrew.devels@gmail.com>
2019-12-03 23:50:37 +0500

    feat(youtube-api): draft for token factory

--- packages/youtube-api/lib/factories/token.factory.dart
+++ packages/youtube-api/lib/factories/token.factory.dart
@@ -1,16 +1,21 @@
 import 'package:http/http.dart';
 
+import 'package:googleapis_auth/auth_io.dart';
 import 'package:googleapis_auth/auth.dart';
+
+import 'package:youtube_api/repositories/token.repository.dart';
 
 typedef ProvidableToken = Future<AccessCredentials> Function(
     Client client, ClientId clientId, List<String> scopes);
 
 class TokenFactory {
-  ProvidableToken getFromConsole() {
-    throw UnimplementedError();
+  ProvidableToken getFromConsole(void Function(String url) prompt) {
+    return (client, clientId, scopes) =>
+        obtainAccessCredentialsViaUserConsent(clientId, scopes, client, prompt)
+            .then((AccessCredentials credentials) => credentials);
   }
 
-  ProvidableToken getFromFileSystem() {
-    throw UnimplementedError();
+  ProvidableToken getFromFileSystem(TokenRepository repository) {
+    return (client, clientId, scopes) => repository.get();
   }
 }
